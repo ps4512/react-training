6f7870dcd06d32a5f5c3abafe192d60269232d34
Tomasz Ducin <tomasz@ducin.it>
2019-10-13 19:52:39 +0200

    redux example test

--- src/store/example.spec.ts
+++ src/store/example.spec.ts
@@ -2,7 +2,7 @@ import { getStore } from './index'
 import { chooseCountry } from './actions'
 
 describe('Redux Store', () => {
-  it('', () => {
+  it('should correctly update after CHOOSE_COUNTRY action dispatched', () => {
     // given
     const store = getStore()
     const initialState = store.getState()
@@ -12,7 +12,14 @@ describe('Redux Store', () => {
 
     // then
     expect(store.getState()).toEqual({
-      
+      "benefits": {},
+      "employees": {
+        "chosenCountry": "UK",
+        "displayedEmployees": [],
+        "employees": null,
+      },
+      "offices": {},
+      "projects": {},
     })
     // no mutation check
     expect(store.getState()).not.toBe(initialState)
